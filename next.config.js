87864ee48ff0e8d70d68f6244fde2c9e65b590d7
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2023-11-10 20:48:08 +0000

    fix: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    images: true, // Add this line if you are using the experimental 'images' feature
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
